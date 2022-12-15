2746f02ba2e7bb89b7828fa032629d9bc24d5bb5
kdtTetz <xenosign@naver.com>
2022-12-16 00:25:56 +0900

    20221216 문제 풀이

--- BJ/1037_약수로 원래 수 구하기.js
+++ BJ/1037_약수로 원래 수 구하기.js
@@ -1,8 +1,8 @@
 // @ts-check
 // https://www.acmicpc.net/problem/1037
 
-// const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
-// const numArr = input[1].split(' ');
+const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+const numArr = input[1].split(' ');
 
 const minNum = Math.min(...numArr);
 const maxNum = Math.max(...numArr);
