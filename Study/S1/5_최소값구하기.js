cdc6e91c128b43b47c7e5b875e0382fc4a9a9b23
xenosign <xenosign@naver.com>
2023-05-23 12:22:20 +0900

    Update 5_최소값구하기.js

--- Study/S1/5_최소값구하기.js
+++ Study/S1/5_최소값구하기.js
@@ -1,11 +1,14 @@
 function solution(arr) {
   let answer = Number.MAX_SAFE_INTEGER;
   answer = Math.min(...arr);
+  const answer2 = Math.max.apply(null, arr);
   // answer = arr[0];
 
   // for (let i = 1; i < arr.length; i++) {
   //   if (answer > arr[i]) answer = arr[i];
   // }
+
+  console.log(answer2);
 
   return answer;
 }
