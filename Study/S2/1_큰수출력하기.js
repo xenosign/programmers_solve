cf55cf226c5d8bb1e44a418271c95e67d1f5495d
xenosign <xenosign@naver.com>
2023-05-30 11:51:40 +0900

    230530

---
+++ Study/S2/1_큰수출력하기.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let answer = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  }
+
+  return answer;
+}
+
+const ex = [6, 7, 3, 9, 5, 6, 12];
+
+console.log(solution(ex));
