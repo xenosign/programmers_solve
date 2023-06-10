94c9272e13aa34616d4d980597aaa7c3df85503a
kdtTetz <xenosign@naver.com>
2023-06-10 21:20:10 +0900

    Update 1_회문문자열.js

--- Study/S3(문자열탐색)/1_회문문자열.js
+++ Study/S3(문자열탐색)/1_회문문자열.js
@@ -1,24 +1,52 @@
-// 내 풀이
+// // 내 풀이
+// function solution(str) {
+//   let answer = 'YES';
+
+//   const lowerCaseStr = str.toLowerCase();
+//   const strArr = lowerCaseStr.split('');
+
+//   const len = strArr.length;
+//   const halfLen = len / 2;
+//   const n = Math.floor(halfLen / 2);
+
+//   for (let i = 0; i < n; i++) {
+//     if (strArr[i] !== strArr[len - i - 1]) {
+//       answer = 'NO';
+//       break;
+//     }
+//   }
+
+//   return answer;
+// }
+
+// // 수업 풀이 1
+// function solution(str) {
+//   const answer = 'YES';
+
+//   const strToLower = str.toLowerCase();
+
+//   const len = strToLower.length;
+
+//   for (let i = 0; i < Math.floor(len / 2); i++) {
+//     if (strToLower[i] !== strToLower[len - i - 1]) return 'NO';
+//   }
+
+//   return answer;
+// }
+
+// 수업 풀이 2
 function solution(str) {
   let answer = 'YES';
 
-  const lowerCaseStr = str.toLowerCase();
-  const strArr = lowerCaseStr.split('');
+  const strToLower = str.toLowerCase();
 
-  const len = strArr.length;
-  const halfLen = len / 2;
-  const n = Math.floor(halfLen / 2);
+  const strToReverse = strToLower.split('').reverse().join('');
 
-  for (let i = 0; i < n; i++) {
-    if (strArr[i] !== strArr[len - i - 1]) {
-      answer = 'NO';
-      break;
-    }
-  }
+  if (strToReverse !== strToLower) answer = 'NO';
 
   return answer;
 }
 
-const ex = 'googGOOd';
+const ex = 'googGOOg';
 
 console.log(solution(ex));
