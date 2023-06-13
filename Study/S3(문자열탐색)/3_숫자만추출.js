7deebdcbe4a65ad173cec47c64b676d41db405af
xenosign <xenosign@naver.com>
2023-06-13 10:24:04 +0900

    Update 3_숫자만추출.js

--- Study/S3(문자열탐색)/3_숫자만추출.js
+++ Study/S3(문자열탐색)/3_숫자만추출.js
@@ -1,11 +1,21 @@
-// 내 풀이
+// // 내 풀이
+// function solution(str) {
+//   let answer = 0;
+
+//   const numStr = str.match(/[0-9]/g).join('');
+//   answer = Number(numStr);
+
+//   return answer;
+// }
+
+// 수업 풀이
 function solution(str) {
-  let answer = 0;
+  let answer = '';
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) answer += str[i];
+  }
 
-  const numStr = str.match(/[0-9]/g).join('');
-  answer = Number(numStr);
-
-  return answer;
+  return parseInt(answer);
 }
 
 const ex = 'g0en2T0s8eSoft';
