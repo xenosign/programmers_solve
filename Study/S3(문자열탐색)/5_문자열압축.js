e145c8718bbb0cc6f7d88388ecb2c92323fbba4f
xenosign <xenosign@naver.com>
2023-06-15 10:56:38 +0900

    Update 5_문자열압축.js

--- Study/S3(문자열탐색)/5_문자열압축.js
+++ Study/S3(문자열탐색)/5_문자열압축.js
@@ -1,20 +1,38 @@
-// 내 풀이
+// // 내 풀이
+// function solution(str) {
+//   let answer = '';
+//   let count = 1;
+//   let flagLetter = '';
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (flagLetter !== str[i]) {
+//       answer += str[i];
+//       flagLetter = str[i];
+//       for (let j = i + 1; j < str.length; j++) {
+//         if (str[i] === str[j]) count++;
+//       }
+//       if (count > 1) {
+//         answer += String(count);
+//         count = 1;
+//       }
+//     }
+//   }
+
+//   return answer;
+// }
+
+// 수업 풀이
 function solution(str) {
   let answer = '';
-  let count = 1;
-  let flagLetter = '';
+  let cnt = 1;
 
   for (let i = 0; i < str.length; i++) {
-    if (flagLetter !== str[i]) {
+    if (str[i] === str[i + 1]) {
+      cnt++;
+    } else {
       answer += str[i];
-      flagLetter = str[i];
-      for (let j = i + 1; j < str.length; j++) {
-        if (str[i] === str[j]) count++;
-      }
-      if (count > 1) {
-        answer += String(count);
-        count = 1;
-      }
+      if (cnt > 1) answer += String(cnt);
+      cnt = 1;
     }
   }
 
