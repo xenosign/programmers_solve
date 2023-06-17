51e3a67eb8e8ab41cd1a483b722348272b5da13f
kdtTetz <xenosign@naver.com>
2023-06-17 18:24:55 +0900

    Update 3_멘토링.js

--- Study/S4(완전탐색)/3_멘토링.js
+++ Study/S4(완전탐색)/3_멘토링.js
@@ -15,6 +15,7 @@ function solution(N, arr) {
 
   console.log(totalRankArr);
 
+  // 배열 완성
   for (let i = 0; i < totalRankArr.length; i++) {
     for (let j = 0; j < totalRankArr[i].length; j++) {
       for (let k = 0; k < totalRankArr.length; k++) {
