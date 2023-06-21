1928b71324af121cb1fef5d17a5066bdc9485e61
xenosign <xenosign@naver.com>
2023-06-21 12:09:13 +0900

    Update 3_멘토링.js

--- Study/S4(완전탐색)/3_멘토링.js
+++ Study/S4(완전탐색)/3_멘토링.js
@@ -1,36 +1,58 @@
-// 내 풀이
+// // 내 풀이
+// function solution(N, arr) {
+//   let answer = 0;
+//   const totalRankArr = [];
+
+//   for (let i = 1; i <= N; i++) {
+//     const rankArr = [];
+//     for (let j = 0; j < arr.length; j++) {
+//       rankArr.push(arr[j].findIndex((el) => el === i) + 1);
+//     }
+//     totalRankArr.push(rankArr);
+//   }
+
+//   let flag = true;
+
+//   for (let i = 0; i < N; i++) {
+//     for (let j = i + 1; j < N; j++) {
+//       flag = true;
+//       for (let k = 0; k < totalRankArr[i].length; k++) {
+//         if (totalRankArr[i][k] < totalRankArr[j][k]) {
+//           flag = false;
+//         }
+//       }
+//       if (flag) answer++;
+//     }
+//   }
+
+//   return answer;
+// }
+
+// 수업 풀이
 function solution(N, arr) {
   let answer = 0;
-  const totalRankArr = [];
+
+  const M = arr.length;
 
   for (let i = 1; i <= N; i++) {
-    const rankArr = [];
-    for (let j = 0; j < arr.length; j++) {
-      rankArr.push(arr[j].findIndex((el) => el === i) + 1);
-    }
-    totalRankArr.push(rankArr);
-  }
-
-  let flag = true;
-
-  console.log(totalRankArr);
-
-  // 배열 완성, 배열 완전 탐색 준비
-  for (let i = 0; i < totalRankArr.length; i++) {
-    for (let j = 0; j < totalRankArr[i].length; j++) {
-      for (let k = 0; k < totalRankArr.length; k++) {
-        if (i === k) continue;
-        if (totalRankArr[i][j] < totalRankArr[k][j]) {
-          console.log(i, j, k, totalRankArr[i][j], totalRankArr[k][j]);
-          flag = false;
+    for (let j = 1; j <= M; j++) {
+      let cnt = 0;
+      for (k = 0; k < M; k++) {
+        let pi = 0;
+        let pj = 0;
+        for (let l = 0; l < N; l++) {
+          if (arr[k][l] === i) pi = l;
+          if (arr[k][l] === j) pj = l;
         }
+        if (pi < pj) cnt++;
       }
+      if (cnt === M) answer++;
     }
-    if (flag) answer++;
   }
 
   return answer;
 }
+
 const N = 4;
 const ex = [
   [3, 4, 1, 2],
