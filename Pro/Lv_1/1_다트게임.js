7d3baefbe75c245298a88fa839567ecf4aebca8c
xenosign <xenosign@naver.com>
2023-06-26 11:27:14 +0900

    Create 1_다트게임.js

---
+++ Pro/Lv_1/1_다트게임.js
@@ -0,0 +1,54 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17682
+
+// 내 풀이
+function solution(dartResult) {
+  let answer = 0;
+  let now = 0;
+  let before = 0;
+  let flag = false;
+
+  let i = 0;
+  while (i < dartResult.length) {
+    let num = 0;
+    num = Number(dartResult[i]);
+    if (dartResult[i] === '1' && dartResult[i + 1] === '0') {
+      num = 10;
+      i++;
+    }
+
+    const con =
+      dartResult[i + 1] === 'S' ? 1 : dartResult[i + 1] === 'D' ? 2 : 3;
+
+    if (dartResult[i + 2] === '*') {
+      before = now;
+      now = num ** con;
+      if (flag) {
+        answer = answer + before * 2 + now * 2;
+      } else {
+        answer = answer + before + now * 2;
+      }
+
+      flag = true;
+      i += 3;
+    } else if (dartResult[i + 2] === '#') {
+      before = now;
+      now = num ** con * -1;
+      answer += now;
+      i += 3;
+    } else {
+      before = now;
+      now = num ** con;
+      answer += now;
+      i += 2;
+    }
+  }
+
+  return answer;
+}
+
+// const tc = '1S2D*3T';
+// const tc = '1D2S#10S';
+// const tc = '1D2S0T';
+const tc = '3D*3T#3D*';
+
+console.log(solution(tc));
