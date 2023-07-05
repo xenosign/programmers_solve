095d7eb5a00a60d423a477bae3a62a07a51d02dc
xenosign <xenosign@naver.com>
2023-07-05 11:11:01 +0900

    크레인 인형

--- Pro/Lv_1/1_크레인인형.js
+++ Pro/Lv_1/1_크레인인형.js
@@ -3,17 +3,58 @@
 function solution(board, moves) {
   let answer = 0;
 
+  const tmpBoard = board;
+  const stack = [];
+
+  const sizeLen = tmpBoard.length;
+  const movesLen = moves.length;
+
+  for (let i = 0; i < movesLen; i++) {
+    for (let j = 0; j < sizeLen; j++) {
+      const pickDoll = tmpBoard[j][moves[i] - 1];
+      // 수업 풀이 push 할 때 비교를 해서 push 안하는 방법으로 개선
+      if (pickDoll !== 0 && pickDoll !== undefined) {
+        if (pickDoll === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else {
+          stack.push(pickDoll);
+        }
+        tmpBoard[j][moves[i] - 1] = 0;
+        break;
+      }
+    }
+
+    // 내 풀이
+    // 일단 push 하고, pop 을 2번 하던 방법
+
+    // const len = stack.length;
+    // if (stack[len - 1] === stack[len - 2] && stack.length > 1) {
+    //   stack.pop();
+    //   stack.pop();
+    //   answer += 2;
+    // }
+  }
+
   return answer;
 }
 
-const board = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 3],
-  [0, 2, 5, 0, 1],
-  [4, 2, 4, 4, 2],
-  [3, 5, 1, 3, 1],
-];
+// const board = [
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 1, 0, 3],
+//   [0, 2, 5, 0, 1],
+//   [4, 2, 4, 4, 2],
+//   [3, 5, 1, 3, 1],
+// ];
 
-const move = [1, 5, 3, 5, 1, 2, 1, 4];
+// const move = [1, 5, 3, 5, 1, 2, 1, 4];
+
+const board = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+const move = [1, 2, 3, 4];
 
 console.log(solution(board, move));
--- Study/S6(자료구조)/3_크레인인형.js
+++ Study/S6(자료구조)/3_크레인인형.js
@@ -2,33 +2,38 @@
 
 function solution(board, moves) {
   let answer = 0;
-  const bucket = [];
 
+  const tmpBoard = board;
+  const stack = [];
+
+  const sizeLen = tmpBoard.length;
   const movesLen = moves.length;
 
-  const boardWithOutZero = [];
-
-  for (let i = 0; i < board.length; i += 1) {
-    const tempArr = board[i].filter((el) => el !== 0);
-    boardWithOutZero.push(tempArr);
-  }
-
-  for (let i = 0; i < movesLen; i += 1) {
-    const currentMoves = moves.shift() - 1;
-
-    if (boardWithOutZero[currentMoves].length > 0) {
-      const currentPick = boardWithOutZero[currentMoves].pop();
-      bucket.push(currentPick);
-      const bucketLen = bucket.length;
-
-      if (bucketLen >= 2) {
-        console.log(bucket);
-        if (bucket[bucketLen - 1] === bucket[bucketLen - 2]) {
-          bucket.splice(bucketLen - 3, 2);
+  for (let i = 0; i < movesLen; i++) {
+    for (let j = 0; j < sizeLen; j++) {
+      const pickDoll = tmpBoard[j][moves[i] - 1];
+      // 수업 풀이 push 할 때 비교를 해서 push 안하는 방법으로 개선
+      if (pickDoll !== 0 && pickDoll !== undefined) {
+        if (pickDoll === stack[stack.length - 1]) {
+          stack.pop();
           answer += 2;
+        } else {
+          stack.push(pickDoll);
         }
+        tmpBoard[j][moves[i] - 1] = 0;
+        break;
       }
     }
+
+    // 내 풀이
+    // 일단 push 하고, pop 을 2번 하던 방법
+
+    // const len = stack.length;
+    // if (stack[len - 1] === stack[len - 2] && stack.length > 1) {
+    //   stack.pop();
+    //   stack.pop();
+    //   answer += 2;
+    // }
   }
 
   return answer;
@@ -42,14 +47,14 @@ function solution(board, moves) {
 //   [3, 5, 1, 3, 1],
 // ];
 
-const board = [
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1],
-];
+// const move = [1, 5, 3, 5, 1, 2, 1, 4];
 
-const move = [1, 5, 3, 5, 1, 2, 1, 4];
+const board = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+const move = [1, 2, 3, 4];
 
 console.log(solution(board, move));
