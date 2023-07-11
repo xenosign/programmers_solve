98fb269dbf2f7957ef8fba103e5037ae8ecae6fa
xenosign <xenosign@naver.com>
2023-07-11 10:49:43 +0900

    Update 2_기능개발.js

--- Pro/Lv_2/2_기능개발.js
+++ Pro/Lv_2/2_기능개발.js
@@ -3,30 +3,33 @@
 function solution(progresses, speeds) {
   const answer = [];
   const processArr = progresses;
-  let tmpCount = 0;
-  let totalCount = 0;
+  let idx = 0;
 
-  for (let day = 1; day < Number.MAX_SAFE_INTEGER; day++) {
+  while (idx < processArr.length) {
     for (let i = 0; i < processArr.length; i++) {
       processArr[i] += speeds[i];
     }
 
-    for (let j = 0; j < processArr.length; j++) {
-      if (processArr[j] >= 100) {
-        console.log(processArr);
-        processArr[j] = Number.MIN_SAFE_INTEGER;
-        tmpCount++;
-        totalCount++;
+    let doneProress = 0;
+
+    for (let i = idx; i < processArr.length; i++) {
+      if (processArr[i] >= 100) {
+        doneProress++;
+        idx++;
+      } else {
+        break;
       }
     }
-    if (tmpCount > 0) answer.push(tmpCount);
-    if (totalCount === processArr.length) break;
+
+    if (doneProress !== 0) answer.push(doneProress);
   }
 
   return answer;
 }
 
-const tc1 = [93, 30, 55];
-const tc2 = [1, 30, 5];
+// const tc1 = [93, 30, 55];
+// const tc2 = [1, 30, 5];
+const tc1 = [95, 90, 99, 99, 80, 99];
+const tc2 = [1, 1, 1, 1, 1, 1];
 
 console.log(solution(tc1, tc2));
