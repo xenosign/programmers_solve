8f58da13135090c52b043218d022c2b2bff63b9f
kdtTetz <xenosign@naver.com>
2023-07-16 03:42:57 +0900

    Update 2_할인행사.js

--- Pro/Lv_2/2_할인행사.js
+++ Pro/Lv_2/2_할인행사.js
@@ -1,7 +1,18 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/131127
 
-function solution(msg) {
+function solution(want, number, discount) {
   let answer = 0;
+
+  const copyNumber = number.slice();
+
+  for (let i = 0; i < discount.length; i++) {
+    const idx = want.indexOf(discount[i]);
+    if (idx !== -1) copyNumber[want.indexOf(discount[i])] -= 1;
+    const delIdx = want.indexOf(discount[i - 10]);
+    if (delIdx !== -1 && i >= 10)
+      copyNumber[want.indexOf(discount[i - 10])] += 1;
+    if (copyNumber.filter((el) => el <= 0).length === want.length) answer++;
+  }
 
   return answer;
 }
