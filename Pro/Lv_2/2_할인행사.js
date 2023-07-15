be445dbf5d4e170b54e3131a59c1fe4b49b5d634
kdtTetz <xenosign@naver.com>
2023-07-15 10:39:15 +0900

    Create 2_할인행사.js

---
+++ Pro/Lv_2/2_할인행사.js
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/131127
+
+function solution(msg) {
+  let answer = 0;
+
+  return answer;
+}
+
+const want = ['banana', 'apple', 'rice', 'pork', 'pot'];
+const number = [3, 2, 2, 2, 1];
+const discount = [
+  'chicken',
+  'apple',
+  'apple',
+  'banana',
+  'rice',
+  'apple',
+  'pork',
+  'banana',
+  'pork',
+  'rice',
+  'pot',
+  'banana',
+  'apple',
+  'banana',
+];
+
+console.log(solution(want, number, discount));
