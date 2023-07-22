ebfee82b26527acbb6256cb856669978a93cb14d
kdtTetz <xenosign@naver.com>
2023-07-22 14:19:21 +0900

    Create 2_프로세스.js

---
+++ Pro/Lv_2/2_프로세스.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+function solution(priorities, location) {
+  let answer = 0;
+
+  return answer;
+}
+
+const priorities = 2;
+const location = [2, 1, 3, 2];
+
+console.log(solution(priorities, location));
