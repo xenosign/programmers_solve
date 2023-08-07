44a4e013dd531d0dfdb7bceadc2f2bb037b59681
xenosign <xenosign@naver.com>
2023-08-07 12:26:39 +0900

    Create 10_순열구하기.js

---
+++ Study/S8_재귀함수와깊이우선탐색(DFS)/10_순열구하기.js
@@ -0,0 +1,29 @@
+function solution(nums, count) {
+  const answer = [];
+  const n = nums.length;
+  const checkArr = Array.from({ length: n }, () => 0);
+  const tmp = Array.from({ length: count }, () => 0);
+
+  function DFS(L) {
+    if (L === count) answer.push(tmp.slice());
+    else {
+      for (let i = 0; i < nums.length; i++) {
+        if (checkArr[i] === 0) {
+          checkArr[i] = 1;
+          tmp[L] = nums[i];
+          DFS(L + 1);
+          checkArr[i] = 0;
+        }
+      }
+    }
+  }
+
+  DFS(0);
+
+  return answer;
+}
+
+const nums = [3, 6, 9];
+const count = 2;
+
+console.log(solution(nums, count));
