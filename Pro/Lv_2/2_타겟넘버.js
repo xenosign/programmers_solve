0ec868d705110666ba51bbed594dd0f116f5cf63
kdtTetz <xenosign@naver.com>
2023-08-15 13:44:09 +0900

    Create 2_타겟넘버.js

---
+++ Pro/Lv_2/2_타겟넘버.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43165
+
+function solution(numbers, target) {
+  let answer = 0;
+
+  return answer;
+}
+
+const numbers = [1, 1, 1, 1, 1];
+const target = 3;
+
+console.log(solution(numbers, target));
