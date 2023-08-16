07fd314c54324f67daedf610060e9965a8dafa9a
kdtTetz <xenosign@naver.com>
2023-08-16 21:41:49 +0900

    Update 2_타겟넘버.js

--- Pro/Lv_2/2_타겟넘버.js
+++ Pro/Lv_2/2_타겟넘버.js
@@ -2,11 +2,32 @@
 
 function solution(numbers, target) {
   let answer = 0;
+  let ari = '';
+
+  function DFS(L, sum, arith) {
+    ari += arith;
+    if (L > numbers.length) {
+      ari = '';
+      return 0;
+    }
+    if (L === numbers.length && sum === target) {
+      console.log(L, sum, ari);
+      answer++;
+    }
+
+    DFS(L + 1, (sum += numbers[L]), '+');
+    DFS(L + 1, (sum -= numbers[L]), '-');
+  }
+
+  DFS(0, 0);
 
   return answer;
 }
 
 const numbers = [1, 1, 1, 1, 1];
 const target = 3;
+
+// const numbers = [4, 1, 2, 1];
+// const target = 4;
 
 console.log(solution(numbers, target));
