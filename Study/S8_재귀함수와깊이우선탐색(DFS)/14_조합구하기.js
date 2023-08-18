6cb408c812db033d055b6d3471dae01f842890a3
xenosign <xenosign@naver.com>
2023-08-18 12:56:07 +0900

    Update 14_조합구하기.js

--- Study/S8_재귀함수와깊이우선탐색(DFS)/14_조합구하기.js
+++ Study/S8_재귀함수와깊이우선탐색(DFS)/14_조합구하기.js
@@ -1,4 +1,5 @@
-// 무한 재귀 버전, 계산할 때마다 모든 트리의 값을 카운팅 함
+// 조합과 순열을 다르다
+// 조합은 순서 상관이 X, 순열은 순서 상관 O 따라서 순열에서는 checkArr 사용
 function solution(n, m) {
   const answer = [];
   const tmpArr = [];
@@ -7,7 +8,10 @@ function solution(n, m) {
     tmpArr.push(0);
   }
 
+  // s 는 스타트 넘버, 1 에서 2 ~ 4 를 조합 / 2 에서 3 ~ 4를 조합 / 3 에서 4를 조합
+  // 조합이다 보니, 순서 상관이 없으므로 이처럼 처리 가능
   function DFS(L, s) {
+    // 몇개 뽑는지 까지 도착 하면 뽑힌 애들 푸쉬
     if (L === m) {
       answer.push(tmpArr.slice());
     } else {
