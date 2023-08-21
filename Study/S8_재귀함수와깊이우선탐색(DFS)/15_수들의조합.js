b752a4867d4e96dac8fedaa488c2061997ca38ad
xenosign <xenosign@naver.com>
2023-08-21 12:17:53 +0900

    Update 15_수들의조합.js

--- Study/S8_재귀함수와깊이우선탐색(DFS)/15_수들의조합.js
+++ Study/S8_재귀함수와깊이우선탐색(DFS)/15_수들의조합.js
@@ -1,24 +1,45 @@
+// // 내 풀이, 이전 조합 코드를 변경
+// function solution(nums, m, mul) {
+//   let answer = 0;
+//   const tmpArr = [];
+
+//   for (let i = 0; i < m; i++) {
+//     tmpArr.push(0);
+//   }
+
+//   function DFS(L, s) {
+//     if (L === m) {
+//       const sum = tmpArr.reduce((acc, cur) => acc + cur);
+//       if (sum % mul === 0) answer++;
+//     } else {
+//       for (let i = s; i < nums.length; i++) {
+//         tmpArr[L] = nums[i];
+//         DFS(L + 1, i + 1);
+//       }
+//     }
+//   }
+
+//   DFS(0, 0);
+
+//   return answer;
+// }
+
+// 수업 풀이
+// 매개 변수로 sum 을 넘겨서 reduce 를 사용하지 않고 합산을 바로 처리
 function solution(nums, m, mul) {
   let answer = 0;
-  const tmpArr = [];
 
-  for (let i = 0; i < m; i++) {
-    tmpArr.push(0);
-  }
-
-  function DFS(L, s) {
-    if (L === m) {
-      const sum = tmpArr.reduce((acc, cur) => acc + cur);
-      if (sum % mul === 0) answer++;
+  function DFS(L, s, sum) {
+    if (L === m && sum % mul === 0) {
+      answer++;
     } else {
       for (let i = s; i < nums.length; i++) {
-        tmpArr[L] = nums[i];
-        DFS(L + 1, i + 1);
+        DFS(L + 1, i + 1, sum + nums[i]);
       }
     }
   }
 
-  DFS(0, 0);
+  DFS(0, 0, 0);
 
   return answer;
 }
