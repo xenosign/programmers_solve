826c92770e72d62f418b8fb8d7e084990482bc4f
xenosign <xenosign@naver.com>
2023-09-05 12:59:21 +0900

    Update 2_게임맵최단거리.js

--- Pro/Lv_2/2_게임맵최단거리.js
+++ Pro/Lv_2/2_게임맵최단거리.js
@@ -4,12 +4,40 @@
 function solution(maps) {
   let answer = -1;
 
-  const queue = [];
+  const xSize = maps[0].length;
+  const ySize = maps.length;
+  const dx = [0, 0, 1, -1];
+  const dy = [-1, 1, 0, 0];
 
-  for (let i = 0; )
+  const queue = [];
+  queue.push([0, 0, 1]);
+
+  while (queue.length) {
+    const [curX, curY, move] = queue.shift();
+
+    if (curX === xSize - 1 && curY === ySize - 1) answer = move;
+
+    for (let i = 0; i < dx.length; i++) {
+      const nx = curX + dx[i];
+      const ny = curY + dy[i];
+
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < xSize &&
+        ny < ySize &&
+        maps[ny][nx] === 1
+      ) {
+        queue.push([nx, ny, move + 1]);
+        maps[ny][nx] = 0;
+      }
+    }
+  }
 
   return answer;
 }
+
+// DFS 풀이
 
 const map1 = [
   [1, 0, 1, 1, 1],
@@ -19,12 +47,12 @@ const map1 = [
   [0, 0, 0, 0, 1],
 ];
 
-const map2 = [
-  [1, 0, 1, 1, 1],
-  [1, 0, 1, 0, 1],
-  [1, 0, 1, 1, 1],
-  [1, 1, 1, 0, 0],
-  [0, 0, 0, 0, 1],
-];
+// const map2 = [
+//   [1, 0, 1, 1, 1],
+//   [1, 0, 1, 0, 1],
+//   [1, 0, 1, 1, 1],
+//   [1, 1, 1, 0, 0],
+//   [0, 0, 0, 0, 1],
+// ];
 
 console.log(solution(map1));
