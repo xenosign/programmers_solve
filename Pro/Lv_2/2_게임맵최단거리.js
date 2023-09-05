7712a4637eadeae831e67fbf8c5ec8297ef98230
xenosign <xenosign@naver.com>
2023-09-05 12:20:50 +0900

    Create 2_게임맵최단거리.js

---
+++ Pro/Lv_2/2_게임맵최단거리.js
@@ -0,0 +1,30 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1844
+
+// BFS 풀이
+function solution(maps) {
+  let answer = -1;
+
+  const queue = [];
+
+  for (let i = 0; )
+
+  return answer;
+}
+
+const map1 = [
+  [1, 0, 1, 1, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 0, 1],
+  [0, 0, 0, 0, 1],
+];
+
+const map2 = [
+  [1, 0, 1, 1, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 1],
+];
+
+console.log(solution(map1));
