e461ed90e5a04d23e18cb9a0ebf65bd8b2dfe89f
xenosign <xenosign@naver.com>
2023-09-08 14:08:41 +0900

    Create 2_프렌즈4블록.js

---
+++ Pro/Lv_2/2_프렌즈4블록.js
@@ -0,0 +1,52 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17679
+
+function solution(m, n, board) {
+  let answer = 0;
+
+  const boardArr = [];
+
+  for (let i = 0; i < board.length; i++) {
+    boardArr.push(board[i].split(''));
+  }
+
+  let delPosObj = {};
+  let delPosArr = [];
+
+  for (let y = 0; y < m - 1; y++) {
+    for (let x = 0; x < n - 1; x++) {
+      const character = boardArr[y][x];
+      if (
+        boardArr[y][x + 1] === character &&
+        boardArr[y + 1][x] === character &&
+        boardArr[y + 1][x + 1] === character
+      ) {
+        delPosObj[`${y},${x}`] = 1;
+        delPosObj[`${y + 1},${x}`] = 1;
+        delPosObj[`${y},${x + 1}`] = 1;
+        delPosObj[`${y + 1},${x + 1}`] = 1;
+      }
+    }
+  }
+
+  answer += Object.keys(delPosObj).length;
+  delPosArr = Object.keys(delPosObj);
+
+  for (let i = 0; i < delPosArr.length; i++) {
+    const [strY, strX] = delPosArr[i].split(',');
+    const y = Number(strY);
+    const x = Number(strX);
+    boardArr[y].splice(x, 1);
+  }
+
+  console.log(boardArr);
+
+  delPosObj = {};
+
+  return answer;
+}
+
+const x = 6;
+const y = 6;
+const board = ['TTTANT', 'RRFACC', 'RRRFCC', 'TRRRAA', 'TTMMMF', 'TMMTTJ'];
+
+console.log(solution(y, x, board));
