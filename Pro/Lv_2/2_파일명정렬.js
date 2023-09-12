b14fe8b5137c32eda05da93fad4cffe9add311aa
xenosign <xenosign@naver.com>
2023-09-12 12:27:12 +0900

    Create 2_파일명정렬.js

---
+++ Pro/Lv_2/2_파일명정렬.js
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17686
+
+function solution(files) {
+  const answer = [];
+  return answer;
+}
+
+const files = [
+  'img12.png',
+  'img10.png',
+  'img02.png',
+  'img1.png',
+  'IMG01.GIF',
+  'img2.JPG',
+];
+
+console.log(solution(files));
