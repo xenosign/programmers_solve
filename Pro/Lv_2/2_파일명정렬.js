1db56d27d2d56d8a56a05829e741d1c728c7f179
xenosign <xenosign@naver.com>
2023-09-12 13:33:43 +0900

    Update 2_파일명정렬.js

--- Pro/Lv_2/2_파일명정렬.js
+++ Pro/Lv_2/2_파일명정렬.js
@@ -2,6 +2,33 @@
 
 function solution(files) {
   const answer = [];
+
+  const orderArr = [];
+
+  const strRegex = /[^0-9]/g;
+  const numRegex = /[0-9]/g;
+  const numRegexWithoutZero = /[0]/g;
+
+  files.forEach((el) => {
+    const [front, back] = el.split('.');
+    const name = front.replace(numRegex, '');
+    const head = name.replace(numRegex, '').toUpperCase();
+    const number = Number(
+      front.replace(strRegex, '').replace(numRegexWithoutZero, ''),
+    );
+    orderArr.push([head, number, back, name]);
+  });
+
+  orderArr.sort((a, b) => a[0].localeCompare(b[0]));
+  orderArr.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return 0;
+  });
+
+  console.log(orderArr);
+
   return answer;
 }
 
@@ -13,5 +40,12 @@ const files = [
   'IMG01.GIF',
   'img2.JPG',
 ];
+
+// const files = [
+//   'F-5 Freedom Fighter',
+//   'B-50 Superfortress',
+//   'A-10 Thunderbolt II',
+//   'F-14 Tomcat',
+// ];
 
 console.log(solution(files));
