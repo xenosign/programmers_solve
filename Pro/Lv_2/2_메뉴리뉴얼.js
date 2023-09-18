02db138c5bfbf44d649adbc7a0a791e97c80c385
xenosign <xenosign@naver.com>
2023-09-18 15:09:28 +0900

    Create 2_메뉴리뉴얼.js

---
+++ Pro/Lv_2/2_메뉴리뉴얼.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/72411
+
+function solution(orders, course) {
+  const answer = [];
+
+  console.log(orders, course);
+
+  return answer;
+}
+
+const orders = ['ABCFG', 'AC', 'CDE', 'ACDE', 'BCFG', 'ACDEH'];
+const course = [2, 3, 4];
+
+console.log(solution(orders, course));
