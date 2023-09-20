387296c618f48c35c4834916554b5c4785caefb3
xenosign <xenosign@naver.com>
2023-09-20 15:28:40 +0900

    Create 2_성격유형검사하기.js

---
+++ Pro/Lv_2/2_성격유형검사하기.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/118666
+
+function solution(survey, choices) {
+  let answer = '';
+
+  return answer;
+}
+
+const survey = ['AN', 'CF', 'MJ', 'RT', 'NA'];
+const choices = [5, 3, 2, 7, 5];
+
+console.log(solution(survey, choices));
